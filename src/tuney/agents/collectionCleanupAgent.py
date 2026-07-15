a63eb4ba20a2e77b285fb7d3e118e413a8775102
osereme <reme.ibazebo@gmail.com>
2026-07-15 13:30:52 -0400

    Build out the collection cleanup agent

--- src/tuney/agents/collectionCleanupAgent.py
+++ src/tuney/agents/collectionCleanupAgent.py
@@ -1,10 +1,27 @@
-from langchain.tools import tool
-from tuney import config, library
+from tuney import config
 from tuney.agents.Agent import Agent
-import json
-from os import fsdecode
+from langchain.agents.middleware import HumanInTheLoopMiddleware
+
+from tuney.agents.tools import remove_item, find_duplicates, locate_file, item_information, search_collection, distinct_values
 
 SYSTEM_PROMPT = """
 Agent focused on the organization and ordiness of the user's collection
 """
 
+_TOOLS = [
+    remove_item,find_duplicates, locate_file, item_information, search_collection, distinct_values
+]
+
+collection_cleanup_agent = Agent (
+    model= lambda: config.get_config().chat_model,
+    system_prompt=SYSTEM_PROMPT,
+    tools=_TOOLS,
+    middleware=[
+        HumanInTheLoopMiddleware(
+            interrupt_on= {
+                "remove_item": {"allowed_decisions": ["approve", "edit","reject"]}
+            },
+            description_prefix="Delete Tool requires approval"
+        )
+    ]
+)
